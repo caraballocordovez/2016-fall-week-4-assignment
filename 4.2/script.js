f26ee57ecebb4af37e5ac04b5374b0a9456c3e0c
Jorge Caraballo Cordovez <jorgecaraballocordovez@MacBook-Pro-de-Jorge.local>
2016-10-08 21:13:16 -0400

    still not working

--- 4.2/script.js
+++ 4.2/script.js
@@ -6,6 +6,17 @@
 
 function reverseArray(arr){
     //your code here
+   var rev = new Array
+    for (var i = arr.length-1; i <=0; i--) {
+    	rev.push(arr[i]);
+    }
+    return rev 
+
 }
 
+
 console.log( reverseArray([1,2,3,4,5]) ); //should log [5,4,3,2,1]
+
+/* arr1 = [1, 2, 4, 5]
+
+console.log(arr1.reverse()) */
